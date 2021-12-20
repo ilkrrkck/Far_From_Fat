e34153d5ce0ea9bdbd985de2849b4caf900b269b
İlker Küçücük <ilker.kck@outlook.com>
2021-12-20 23:50:06 +0300

    Firebase de veritabanı oluşturuldu FireStore projeye entegre edildi Verilen çekilmesi sağlandı

--- app/src/main/java/com/ilkrrkck/farfromfat/AnaMenu.java
+++ app/src/main/java/com/ilkrrkck/farfromfat/AnaMenu.java
@@ -3,35 +3,84 @@ package com.ilkrrkck.farfromfat;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.Toast;
 
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class AnaMenu extends AppCompatActivity {
 
+    private final String admin_Mail = "ilker.kck@outlook.com";
+    private final String admin_Sifre = "159753";
     private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    private FirebaseFirestore mFireStore;
+    private DocumentReference docRef;
+    private CollectionReference colRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        /****************FİREBASE AUTH.**************************/
         mAuth = FirebaseAuth.getInstance();
-        mAuth.createUserWithEmailAndPassword("ilker.kck@outlook.com","159753")
-                .addOnFailureListener(this, new OnFailureListener() {
+        mFireStore=FirebaseFirestore.getInstance();
+        mUser=mAuth.getCurrentUser();
+        GirisYap();
+
+        VerileriGetir();
+
+
+    }
+
+    private void VerileriGetir(){
+        //colRef=mFireStore.collection("Program1_Gun1").document("BenchPress").collection("Tekrar"); //COL İÇİ VERİLER
+        docRef=mFireStore.collection("Program1_Gun1").document("BenchPress"); // ŞİMDİLİK SADECE BENCHPRESS VERİLERİ collları GETİRDİ
+        docRef.get()
+                .addOnSuccessListener(this, new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(@NonNull DocumentSnapshot documentSnapshot) {
+                        if(documentSnapshot.exists()){
+                            System.out.println("VERİLER  "+documentSnapshot.getData());
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(AnaMenu.this,"FİREBASE HATA VERDİ",Toast.LENGTH_LONG).show();
+                        Toast.makeText(AnaMenu.this,e.getMessage(),Toast.LENGTH_LONG).show();
                     }
                 });
-        /****************FİREBASE AUTH.**************************/
+    }
+
+    private void GirisYap(){
+
+        mAuth.signInWithEmailAndPassword(admin_Mail,admin_Sifre)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                            Toast.makeText(AnaMenu.this,"Giriş Başarılı",Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(AnaMenu.this,task.getException().getMessage(),Toast.LENGTH_LONG).show();
+
+                    }
+                });
 
 
 
